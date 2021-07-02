25909177bdd61d3eefcabd78d142350ab82ff1b8
Dragan Bjedov <draganbjedov@gmail.com>
2021-07-02 15:28:26 +0200

    Fixed navigation for work

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/work/<int:work_id>')
+def work(work_id):
+    return render_template(f'work.html', id = work_id)
 
 @app.route('/<string:page_name>')
 def page(page_name):
