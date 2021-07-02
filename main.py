fb20f2e71a6866a17ef49bd9cf208092846eff85
Dragan Bjedov <draganbjedov@gmail.com>
2021-07-02 14:49:33 +0200

    One function to handle navigation

--- main.py
+++ main.py
@@ -8,24 +8,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/works')
-def works():
-    return render_template('works.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/work')
-def work():
-    return render_template('work.html')
+@app.route('/<string:page_name>')
+def page(page_name):
+    return render_template(f'{page_name}.html')
 
 
 if __name__ == '__main__':
