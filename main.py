418df7a2505c923d52165ab0c8e716263b1ef9dc
Dragan Bjedov <draganbjedov@gmail.com>
2021-07-02 17:00:22 +0200

    Collect contact form data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
@@ -7,13 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/work/<int:work_id>')
 def work(work_id):
-    return render_template(f'work.html', id = work_id)
+    return render_template(f'work.html', id=work_id)
+
 
 @app.route('/<string:page_name>')
 def page(page_name):
     return render_template(f'{page_name}.html')
+
+
+@app.route('/contact_form', methods=['POST'])
+def contact_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+    return redirect(
+        url_for(endpoint='.page', page_name='contact_ok', contact_name=name))
 
 
 if __name__ == '__main__':
