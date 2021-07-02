1985e6c736420998b7e5d4376e2448122574549f
Dragan Bjedov <draganbjedov@gmail.com>
2021-07-02 14:25:04 +0200

    Fixed navigation

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/works')
+def works():
+    return render_template('works.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
 
 
 if __name__ == '__main__':
